b01892c7cf9a0cb61daf5ee7f1c258a453400c37
Tom Smale <tomsmale@protonmail.com>
2025-01-05 15:53:37 +0000

    refactored cellular automata

--- src/main.cc
+++ src/main.cc
@@ -5,7 +5,12 @@
 #include <raylib.h>
 #include "raygui.h"
 #include <memory>
-#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <array>
+#include <cstdint>
+#include <nlohmann/json.hpp>
+#include <fstream>
 
 enum class GameState {
   kMainMenu,
@@ -20,46 +25,217 @@ enum class PlayState {
   kPaused,
 };
 
-/*class Particle {
- public:
-  enum class Type {
-    kVoid,
-    kAir,
-    kSand,
-    kDirt,
-    kStone,
-    kWater,
-    kLava,
-    kCount,
-  };
- private:
-};*/
-
-enum class Particle {
-  kVoid,
-  kAir,
-  kSand,
-  kDirt,
-  kStone,
-  kWater,
-  kLava,
-  kBedrock,
-  kCount,
-};
-
 Color particleColors[] = {
-    BLANK, // VOID
     BLACK, // AIR
     BEIGE, // SAND
-    BROWN, // DIRT
     GRAY,  // STONE
     BLUE,  // WATER
-    RED,   // LAVA
     DARKGRAY, // BEDROCK
 };
 
-class World {
+class Cell {
+public:
+  enum class Element : uint8_t {
+    kAir,
+    kSand,
+    kStone,
+    kWater,
+    //kLava,
+    //kDirt,
+    kBedrock,
+    kCount,
+  };
 
+  enum class Type : uint8_t {
+    kEmpty,
+    kPowder,
+    kSolid,
+    kLiquid,
+    kFire,
+    kGas,
+  };
+
+  static void LoadElements(const std::string& filename) {
+    std::ifstream file(filename);
+    if (!file.is_open()) {
+      throw std::runtime_error("Failed to open file: " + filename);
+    }
+
+    nlohmann::json json;
+    file >> json;
+
+    if (!json.contains("elements") || !json["elements"].is_array()) {
+      throw std::runtime_error("Invalid JSON config format: " + filename);
+    }
+    if (json["elements"].size() != static_cast<size_t>(Cell::Element::kCount)-1) {
+      throw std::runtime_error("Invalid number of elements in JSON config: " + filename);
+    }
+
+    for (const auto& element : json["elements"]) {
+      auto i = static_cast<size_t>(element["index"]);
+      types[i] = static_cast<Cell::Type>(element["type"]);
+      weights[i] = element["weight"];
+      viscosity[i] = element["viscosity"];
+      names[i] = element["name"];
+      colors[i] = particleColors[i];
+    }
+  }
+
+  static Type GetType(Element element) {
+    return types[static_cast<size_t>(element)];
+  }
+
+  static Color GetColor(Element element) {
+    return colors[static_cast<size_t>(element)];
+  }
+
+  static int GetWeight(Element element) {
+    return weights[static_cast<size_t>(element)];
+  }
+
+  static int GetViscosity(Element element) {
+    return viscosity[static_cast<size_t>(element)];
+  }
+
+  static std::string GetName(Element element) {
+    return names[static_cast<size_t>(element)];
+  }
+
+private:
+  static std::array<Type,        static_cast<size_t>(Element::kCount)> types;
+  static std::array<Color,       static_cast<size_t>(Element::kCount)> colors;
+  static std::array<int,         static_cast<size_t>(Element::kCount)> weights;
+  static std::array<int,         static_cast<size_t>(Element::kCount)> viscosity;
+  static std::array<std::string, static_cast<size_t>(Element::kCount)> names;
+};
+
+class AutomataMatrix {
+public:
+  AutomataMatrix(int width = 400, int height = 300) : width(width), height(height) {
+    cell.resize(width * height);
+    heat.resize(width * height);
+    shade.resize(width * height);
+    dirty.resize(width * height);
+
+    for (int y = 0; y < height; y++) {
+      for (int x = 0; x < width; x++) {
+        if (x == 0 || x == width - 1 || y == 0 || y == height - 1) {
+          cell[y*width + x] = Cell::Element::kBedrock;
+        } else {
+          cell[y*width + x] = Cell::Element::kAir;
+        }
+
+        heat[y*width + x] = 0;
+        shade[y*width + x] = 0;
+        dirty[y*width + x] = 1;
+      }
+    }
+  }
+
+  [[nodiscard]] inline int GetWidth()  const { return width; }
+  [[nodiscard]] inline int GetHeight() const { return height; }
+
+  [[nodiscard]] inline int Above     (const int pos) const { return pos + width; }
+  [[nodiscard]] inline int Below     (const int pos) const { return pos - width; }
+  [[nodiscard]] inline int Right     (const int pos) const { return pos + 1; }
+  [[nodiscard]] inline int Left      (const int pos) const { return pos - 1; }
+  [[nodiscard]] inline int AboveRight(const int pos) const { return pos + width + 1; }
+  [[nodiscard]] inline int AboveLeft (const int pos) const { return pos + width - 1; }
+  [[nodiscard]] inline int BelowRight(const int pos) const { return pos - width + 1; }
+  [[nodiscard]] inline int BelowLeft (const int pos) const { return pos - width - 1; }
+
+  void SetCell(const int pos, const Cell::Element element) {
+    cell[pos] = element;
+  }
+
+  void SetCell(const int x, const int y, const Cell::Element element) {
+    cell[y*width + x] = element;
+  }
+
+  void SwapCells(const int pos1, const int pos2) {
+    std::swap(cell[pos1], cell[pos2]);
+  }
+
+  void SwapCells(const int x1, const int y1, const int x2, const int y2) {
+    std::swap(cell[y1*width + x1], cell[y2*width + x2]);
+  }
+
+  void ApplyGravity(int pos, Cell::Element element, const int direction, const bool liquid) {
+    int weight = Cell::GetWeight(element);
+    while (weight-- != 0) {
+      const int below = Below(pos);
+      const int directionA = direction ? BelowRight(pos) : BelowLeft(pos);
+      const int directionB = direction ? BelowLeft(pos) : BelowRight(pos);
+      if (Cell::GetType(cell[below]) == Cell::Type::kEmpty) {
+        SwapCells(pos, below);
+        pos = below;
+      } else if (Cell::GetType(cell[directionA]) == Cell::Type::kEmpty) {
+        SwapCells(pos, directionA);
+        pos = directionA;
+      } else if (Cell::GetType(cell[directionB]) == Cell::Type::kEmpty) {
+        SwapCells(pos, directionB);
+        pos = directionB;
+      } else {
+        if (liquid) {
+          ApplySpread(pos, weight, direction);
+        }
+        break;
+      }
+    }
+    dirty[pos] = 0;
+  }
+
+  void ApplySpread(int& pos, int spread, const int direction) {
+    while (spread-- != 0) {
+      const int directionA = direction ? Right(pos) : Left(pos);
+      const int directionB = direction ? Left(pos) : Right(pos);
+      if (Cell::GetType(cell[directionA]) == Cell::Type::kEmpty) {
+        SwapCells(pos, directionA);
+        pos = directionA;
+      } else if (Cell::GetType(cell[directionB]) == Cell::Type::kEmpty) {
+        SwapCells(pos, directionB);
+        pos = directionB;
+      }
+    }
+  }
+
+  void Update() {
+    const int direction = GetRandomValue(0, 1);
+    for (int i = 0; i < width*height; i++) {
+      switch(cell[i]) {
+        case Cell::Element::kSand: {
+          ApplyGravity(i, Cell::Element::kSand, direction, false);
+          break;
+        }
+        case Cell::Element::kWater: {
+          ApplyGravity(i, Cell::Element::kWater, direction, true);
+          break;
+        }
+        default:
+          break;
+      }
+    }
+    std::ranges::fill(dirty, 1);
+  }
+
+private:
+  int width;
+  int height;
+
+  std::vector<Cell::Element> cell;
+  std::vector<uint8_t>       heat;
+  std::vector<uint8_t>       shade;
+  std::vector<uint8_t>       dirty;
+};
+
+
+enum class Particle {
+  kAir,
+  kSand,
+  kStone,
+  kWater,
+  kBedrock,
+  kCount,
 };
 
 class Application {
@@ -87,7 +263,9 @@ public:
       }
     }
 
-    std::cout << static_cast<int>(world[worldWidth-1]) << std::endl;
+    // allocate and initialize dirty map
+    dirtyMap = std::make_unique<uint8_t[]>(worldWidth * worldHeight);
+    std::fill_n(dirtyMap.get(), worldWidth * worldHeight, 1);
 
     // setup world texture
     pixels = std::make_unique<Color[]>(worldWidth * worldHeight);
@@ -194,7 +372,7 @@ public:
   [[nodiscard]] inline int BelowRight(int pos) const { return pos - worldWidth + 1; }
   [[nodiscard]] inline int BelowLeft (int pos) const { return pos - worldWidth - 1; }
 
-  inline void PlaceParticle(int pos, Particle type) {
+  inline void SetParticle(int pos, Particle type) {
     world[pos] = type;
   }
 
@@ -202,6 +380,40 @@ public:
     world[newPos] = newType;
     world[oldPos] = oldType;
   }
+
+  void ApplyGravity(int pos, Particle type, int weight, int direction) {
+    int tempPos = pos;
+    while (weight-- != 0) {
+      int below = Below(pos);
+      if (world[below] == Particle::kAir) {
+        SwapParticle(pos, Particle::kAir, below, type);
+        tempPos = below;
+        continue;
+      } else {
+        int belowRight = BelowRight(pos);
+        int belowLeft = BelowLeft(pos);
+        if (direction == 0) {
+          if (world[belowRight] == Particle::kAir) {
+            SwapParticle(pos, Particle::kAir, belowRight, type);
+            pos = belowRight;
+          } else if (world[belowLeft] == Particle::kAir) {
+            SwapParticle(pos, Particle::kAir, belowLeft, type);
+            pos = belowLeft;
+          }
+        } else {
+          if (world[belowLeft] == Particle::kAir) {
+            SwapParticle(pos, Particle::kAir, belowLeft, type);
+            pos = belowLeft;
+          } else if (world[belowRight] == Particle::kAir) {
+            SwapParticle(pos, Particle::kAir, belowRight, type);
+            pos = belowRight;
+          }
+        }
+      }
+    }
+  }
+
+
 
   void CalculateGravity(int& pos, Particle type, int& fallFactor, int fallDirection, bool calculateSpread = false) {
     while (fallFactor-- != 0) {
@@ -282,13 +494,13 @@ public:
       Vector2 mousePos = GetMousePosition();
       Vector2 worldPos = ScreenToWorld(mousePos);
       if (world[GetIndex(worldPos.x, worldPos.y)] != Particle::kBedrock) {
-        PlaceParticle(GetIndex(worldPos.x, worldPos.y), Particle::kSand);
+        SetParticle(GetIndex(worldPos.x, worldPos.y), Particle::kSand);
       }
     } else if (IsMouseButtonDown(MOUSE_BUTTON_RIGHT)) {
       Vector2 mousePos = GetMousePosition();
       Vector2 worldPos = ScreenToWorld(mousePos);
       if (world[GetIndex(worldPos.x, worldPos.y)] != Particle::kBedrock) {
-        PlaceParticle(GetIndex(worldPos.x, worldPos.y), Particle::kWater);
+        SetParticle(GetIndex(worldPos.x, worldPos.y), Particle::kWater);
       }
     }
 
@@ -380,6 +592,7 @@ private:
   int worldWidth = 400;
   int worldHeight = 300;
   std::unique_ptr<Particle[]> world;
+  std::unique_ptr<uint8_t[]> dirtyMap;
   std::unique_ptr<Color[]> pixels;
   Image worldImage = {
       .data = nullptr,
